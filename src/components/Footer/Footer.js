6e4a82a0d34054d7400d56dd66ce4ad7143caa6e
Clayton Campos <clayton.almeida.campos@gmail.com>
2021-06-17 10:05:06 -0300

    create section footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,72 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiTwotoneMail, AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { FaWhatsapp } from 'react-icons/fa';
 
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      {/*
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Whatsapp</LinkTitle>
+          <LinkItem href="https://api.whatsapp.com/send?phone=5511947049253">
+            <FaWhatsapp size="3rem" />
+          </LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>E-mail</LinkTitle>
+          <LinkItem href="mailto:clayton.almeida.campos@gmail.com">
+            <AiTwotoneMail size="3rem" />
+          </LinkItem>
+        </LinkColumn>
+         <LinkColumn>
+          <LinkTitle>Linkedin</LinkTitle>
+          <LinkItem href="https://www.linkedin.com/in/clayton-almeida-campos-198732176/">
+            <AiFillLinkedin size="3rem" />
+          </LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>GitHub</LinkTitle>
+          <LinkItem href="https://github.com/claytoncampos">
+            <AiFillGithub size="3rem" />
+          </LinkItem>
+        </LinkColumn> 
+      </LinkList>*/}
+      <LinkList />
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Nunca pare de Estudar</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="mailto:clayton.almeida.campos@gmail.com">
+            <AiTwotoneMail size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://api.whatsapp.com/send?phone=5511947049253">
+            <FaWhatsapp size="3rem" />
+          </SocialIcons>
+
+          <SocialIcons href="https://github.com/claytoncampos">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/clayton-almeida-campos-198732176/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 import {
